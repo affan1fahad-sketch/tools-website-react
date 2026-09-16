12c855373f43067fb049186b3f7ae95f698847ae
Affan Fahad <affanfahad7@gmail.com>
2026-09-16 09:54:57 +0530

    chore: disable all ads temporarily

--- src/components/AdBanner.jsx
+++ src/components/AdBanner.jsx
@@ -1,22 +1,4 @@
-import { useEffect } from "react";
-
+// Ads temporarily disabled
 export default function AdBanner() {
-  useEffect(() => {
-    try {
-      (window.adsbygoogle = window.adsbygoogle || []).push({});
-    } catch (e) {}
-  }, []);
-
-  return (
-    <div className="ad-wrap">
-      <ins
-        className="adsbygoogle"
-        style={{ display: "block" }}
-        data-ad-client="ca-pub-6130200919347577"
-        data-ad-slot="3635053903"
-        data-ad-format="auto"
-        data-full-width-responsive="true"
-      />
-    </div>
-  );
+  return null;
 }
